367fff105bbbb3526fa92da730c737a206e4ce82
dqv2304 <vietdao2304@gmail.com>
2025-03-05 20:39:28 +0700

    Create b7.c

---
+++ baitapltcb/b7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+	float pts;
+	scanf("%f", &pts);
+	if (pts < 5 ) 
+		printf("Yeu !");
+	else if (pts >= 5 && pts < 7)
+		printf("trung binh");
+	else if (pts >= 7 && pts < 8)
+		printf("Kha");
+	else if (pts >= 8 && pts < 9)
+		printf("Gioi");
+	else 
+		printf("xuat sac !");
+		
+	return 0;
+}
