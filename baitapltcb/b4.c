42d1347e15153b7d759df95974cff16aad9bf0ee
dqv2304 <vietdao2304@gmail.com>
2025-03-05 18:22:18 +0700

    Create b4.c

---
+++ baitapltcb/b4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	int R;
+	scanf("%d", &R);
+	double s = (4 * M_PI * R*R);
+	double v = (4 * M_PI * (double)pow(R, 3)/3);
+	printf("Dien tich: %.2lf \n", s);
+	printf("The tich: %.2lf ", v);
+	
+	
+	return 0;
+}
