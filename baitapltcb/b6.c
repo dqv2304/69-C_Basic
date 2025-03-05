3f02d4a3383fff980d98cd5139ca3d58a44d9c4b
dqv2304 <vietdao2304@gmail.com>
2025-03-05 19:44:56 +0700

    info

---
+++ baitapltcb/b6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+	float a,b,c,d,e,f;
+	scanf("%f%f%f%f%f%f", &a, &b, &c, &d,&e,&f);
+	float D = a*e - b*d;
+	float Dx = c*e - b*f;
+	float Dy =  a*f - c*d;
+
+	if (D != 0){
+		float x = Dx / D;
+		float y = Dy / D;
+		printf("Nghiem cua he la: x = %.2f, y = %.2f", x,y);
+	}
+	else {
+		if (Dx == 0 && Dy == 0) {
+			printf("he co vo so nghiem");
+		}
+		else {
+			printf("He vo nghiem");
+		}
+	}
+	return 0;
+}
