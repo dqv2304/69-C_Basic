101d9107e5cd68145ab6e62748c83086053ebb04
dqv2304 <vietdao2304@gmail.com>
2025-03-05 22:58:45 +0700

    info

---
+++ baitapltcb/b8.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+bool check(double a, double b){
+	double eps = 0.000001;
+	return fabs(a-b) < eps;
+}
+
+int main (){
+	double a,b,c;
+	scanf("%lf%lf%lf", &a,&b,&c);
+	if (a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a){
+		double chuvi = a + b + c;
+		double hc = chuvi / 2;
+		double s = sqrt(hc * (hc - a) * (hc - b) * (hc - c));
+			
+		if (check(a, b) && check(a, c)){
+			printf("tam giac deu\n");
+		} 
+		else if (check(a, b) || check(b, c) || check(a, c)){
+			if (check(a * a, b * b + c * c) || check(b * b, a * a + c * c) || check(c * c, a * a + b * b)){
+				printf("tam giac vuong can\n");
+			}
+			else {
+				printf("tam giac can\n");
+			}
+		}
+		else if(check(a * a, b * b + c * c) || check(b*b, a*a+c*c) || check(c*c, a*a+b*b)){
+			printf("tam giac vuong\n");
+		}
+		else {
+			printf("tam giac thuong\n");
+		}
+		
+		printf("chu vi = %.02lf\n", chuvi);
+		printf("dien tich = %.02lf\n", s);
+	}
+	else {
+		printf("khong phai tam giac");
+	}
+
+	return 0;
+}
